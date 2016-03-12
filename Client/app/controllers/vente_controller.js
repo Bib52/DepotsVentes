caec6fc02408f1d40dbd57450fcc09ce9666172c
Florian Bergeret <florianbergeret@hotmail.fr>
2016-03-12 01:16:52 +0100

    Debut generation facture pdf

--- Client/app/controllers/vente_controller.js
+++ Client/app/controllers/vente_controller.js
@@ -43,10 +43,44 @@ angular.module("DepotVente").controller('VenteController',['$scope', 'Vente', 'P
 				console.log($scope.listObjet.objet);
 			}
 
+			$scope.calculDate = function(){
+				var m = [
+					"Janvier", "Fevrier", "Mars",
+					"Avril", "Mai", "Juin", "Juillet",
+					"Aout", "Septembre", "Octobre",
+					"Novembre", "Decembre"];
+				var d = new Date(Date.now());
+				var jour = d.getDate();
+				var mois = d.getMonth();
+				var annee = d.getFullYear();
+				var date = (jour + ' ' + m[mois] + ' ' + annee);
+				return date;
+			}
+
 			$scope.generatePDF = function(){
 				console.log('generer pdf');
-				// var pdf = new JSPF('p','pt','a4');
-				// pdf.save('test.pdf');
+				var date = $scope.calculDate();
+				console.log(date);
+				var facture = new jsPDF();
+				facture.text(20, 20, "Numero de facture : " /*ajout id vente*/);
+				facture.text(20, 30, "Date : " + date);
+				if($scope.vente.nom && $scope.vente.prenom){
+					facture.text(120, 40, $scope.vente.nom + " " + $scope.vente.prenom);
+				}
+				else if($scope.vente.prenom){
+					facture.text(120, 40, $scope.vente.prenom);
+				}
+				else if($scope.vente.nom){
+					facture.text(120, 40, $scope.vente.nom);
+				}
+				if($scope.vente.adresse){
+					facture.text(120, 48, $scope.vente.adresse);
+				}
+				facture.setFontSize(22);
+				facture.text(20, 60, "Recapitulatif : ");
+				facture.setFontSize(16);
+				/* Parcours de $scope.listObjet */
+				facture.save('facture.pdf');
 			}
 
 			$scope.annuleVente = function(){
